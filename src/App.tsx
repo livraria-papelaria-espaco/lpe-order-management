6a16b5d796595ae8d574a15ac387d07212b7ca44
Diogo Correia <me@diogotc.com>
2021-06-25 18:05:11 +0100

    Change theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { SnackbarProvider } from 'notistack';
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import routes from './constants/routes';
 import BookPage from './pages/BookPage';
 import BooksPage from './pages/BooksPage';
@@ -11,22 +12,35 @@ import OrderNewPage from './pages/OrderNewPage';
 import OrdersPage from './pages/OrdersPage';
 import Root from './pages/Root';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#273377',
+    },
+    secondary: {
+      main: '#f39524',
+    },
+  },
+});
+
 export default function App() {
   return (
-    <SnackbarProvider autoHideDuration={8000}>
-      <Router>
-        <Root>
-          <Switch>
-            <Route path={routes.CUSTOMER} component={CustomerPage} />
-            <Route path={routes.CUSTOMERS} component={CustomersPage} />
-            <Route path={routes.BOOK} component={BookPage} />
-            <Route path={routes.BOOKS} component={BooksPage} />
-            <Route path={routes.ORDERS_NEW} component={OrderNewPage} />
-            <Route path={routes.ORDERS} component={OrdersPage} />
-            <Route path={routes.HOME} component={HomePage} />
-          </Switch>
-        </Root>
-      </Router>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider autoHideDuration={8000}>
+        <Router>
+          <Root>
+            <Switch>
+              <Route path={routes.CUSTOMER} component={CustomerPage} />
+              <Route path={routes.CUSTOMERS} component={CustomersPage} />
+              <Route path={routes.BOOK} component={BookPage} />
+              <Route path={routes.BOOKS} component={BooksPage} />
+              <Route path={routes.ORDERS_NEW} component={OrderNewPage} />
+              <Route path={routes.ORDERS} component={OrdersPage} />
+              <Route path={routes.HOME} component={HomePage} />
+            </Switch>
+          </Root>
+        </Router>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
