b588f8a91e64fe9f7122b6810e364c04cf9616ac
Diogo Correia <me@diogotc.com>
2021-06-30 17:44:29 +0100

    Use BackButton component in CustomerPage

--- src/pages/CustomerPage.tsx
+++ src/pages/CustomerPage.tsx
@@ -1,19 +1,12 @@
-import { Button, makeStyles } from '@material-ui/core';
-import BackIcon from '@material-ui/icons/ArrowBackRounded';
 import { useSnackbar } from 'notistack';
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
+import BackButton from '../components/BackButton';
 import CustomerData from '../components/Customer/CustomerPage/CustomerData';
 import Loading from '../components/Loading';
 import { CustomerPage, CustomerQueryResult } from '../types/database';
 
 const { ipcRenderer } = require('electron');
-
-const useStyles = makeStyles((theme) => ({
-  backButton: {
-    marginBottom: theme.spacing(2),
-  },
-}));
 
 type ParamType = {
   id: string | undefined;
@@ -24,7 +17,6 @@ export default function CustomersPage() {
   const [data, setData] = useState<CustomerPage>();
   const history = useHistory();
   const { enqueueSnackbar } = useSnackbar();
-  const classes = useStyles();
 
   useEffect(() => {
     ipcRenderer.send('db-customer-find-one', id);
@@ -45,13 +37,7 @@ export default function CustomersPage() {
 
   return (
     <div>
-      <Button
-        onClick={() => history.goBack()}
-        startIcon={<BackIcon />}
-        className={classes.backButton}
-      >
-        VOLTAR
-      </Button>
+      <BackButton />
       <CustomerData customer={data?.customer} />
     </div>
   );
