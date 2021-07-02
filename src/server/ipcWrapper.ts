df4adb358ae9dd01d488ed2562fa0e75cd30f6e0
Diogo Correia <me@diogotc.com>
2021-07-02 19:11:58 +0100

    Add ipcWrapper backend utility

---
+++ src/server/ipcWrapper.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ipcMain, IpcMainEvent } from 'electron';
+import log from 'electron-log';
+
+export const registerListener = (
+  channel: string,
+  func: (...args: any[]) => any | Promise<any>
+) => {
+  ipcMain.on(channel, async (event: IpcMainEvent, ...args: any[]) => {
+    try {
+      log.debug(`Handling IPC event on channel ${channel}`);
+
+      const result = await func(...args);
+      event.reply(`${channel}-result`, result);
+
+      log.debug(`Finished handling IPC event for channel ${channel}`);
+    } catch (error) {
+      log.error(
+        `An unhandled exception happened while handling channel ${channel}`,
+        error
+      );
+    }
+  });
+};
+
+export default {
+  registerListener,
+};
--- src/server/queries/orderImports.ts
+++ src/server/queries/orderImports.ts
@@ -1,66 +1,65 @@
-import { BrowserWindow, ipcMain, IpcMainEvent } from 'electron';
+import { BrowserWindow } from 'electron';
 import { getMetadataByURL } from '../../utils/bookMetadata';
+import { registerListener } from '../ipcWrapper';
 
-ipcMain.on('order-import-wook-open', (event: IpcMainEvent) => {
-  const window = new BrowserWindow({
-    width: 1024,
-    height: 728,
-    webPreferences: {
-      devTools: false,
-      contextIsolation: true,
-      nodeIntegration: false,
-    },
-  });
+const importFromWook = () =>
+  new Promise((resolve) => {
+    const window = new BrowserWindow({
+      width: 1024,
+      height: 728,
+      webPreferences: {
+        devTools: false,
+        contextIsolation: true,
+        nodeIntegration: false,
+      },
+    });
 
-  window.loadURL('https://www.wook.pt/comprar-manuais-escolares');
+    window.loadURL('https://www.wook.pt/comprar-manuais-escolares');
 
-  window.webContents.session.clearStorageData({
-    storages: ['cookies'],
-  });
+    window.webContents.session.clearStorageData({
+      storages: ['cookies'],
+    });
 
-  window.webContents.executeJavaScript(
-    `document.getElementById("cookieLawBar").style.display = "none"`
-  );
+    window.webContents.executeJavaScript(
+      `document.getElementById("cookieLawBar").style.display = "none"`
+    );
 
-  const intervalId = setInterval(async () => {
-    try {
-      const data = await window.webContents.executeJavaScript(`
+    const intervalId = setInterval(async () => {
+      try {
+        const data = await window.webContents.executeJavaScript(`
     [...document.getElementById("cestoProdutos").getElementsByTagName("li")].map((prod) => {
       return prod.getElementsByClassName("cover")[0].getAttributeNode("onclick").value.match(/\\d+/)[0]
     })
     `);
 
-      if (data.length > 0) {
-        clearInterval(intervalId);
-        event.reply('order-import-wook-open-result', data);
-        window.destroy();
+        if (data.length > 0) {
+          clearInterval(intervalId);
+          resolve(data);
+          window.destroy();
+        }
+      } catch (e) {
+        // Ignore
       }
-    } catch (e) {
-      // Ignore
-    }
-  }, 1000);
+    }, 1000);
 
-  window.on('close', () => {
-    clearInterval(intervalId);
-    event.reply('order-import-wook-open-result', false);
+    window.on('close', () => {
+      clearInterval(intervalId);
+      resolve(false);
+    });
   });
-});
 
-ipcMain.on(
-  'order-import-wook-parse',
-  async (event: IpcMainEvent, books: string[]) => {
-    const booksUnique = [...new Set(books)];
+const parseImportFromWook = async (books: string[]) => {
+  const booksUnique = [...new Set(books)];
 
-    const metadata = await Promise.all(
-      booksUnique.map((url) =>
-        getMetadataByURL(`https://wook.pt/Artigos/?id=${url}`)
-      )
-    );
+  const metadata = await Promise.all(
+    booksUnique.map((url) =>
+      getMetadataByURL(`https://wook.pt/Artigos/?id=${url}`)
+    )
+  );
 
-    event.reply(
-      'order-import-wook-parse-result',
-      // filter out failed books
-      metadata.filter((book) => !!book)
-    );
-  }
-);
+  // filter out failed books
+  return metadata.filter((book) => !!book);
+};
+
+registerListener('order-import-wook-open', importFromWook);
+registerListener('order-import-wook-parse', parseImportFromWook);
